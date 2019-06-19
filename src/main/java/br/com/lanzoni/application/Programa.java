8431c6b79fde42887d9bd8cc88b0e390577e6314
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2019-06-19 18:17:27 -0300

    Exemplo de IoC e DI

--- src/main/java/br/com/lanzoni/application/Programa.java
+++ src/main/java/br/com/lanzoni/application/Programa.java
@@ -1,23 +1,33 @@
 package br.com.lanzoni.application;
 
 import br.com.lanzoni.Pessoa;
+import br.com.lanzoni.domain.Usuario;
+import br.com.lanzoni.service.UsuarioServico;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Scanner;
+
 @Configuration
-@ComponentScan
+@ComponentScan("br.com.lanzoni")
 public class Programa {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Programa.class);
+        Scanner lerDados = new Scanner(System.in);
+        Usuario usuario = new Usuario("Vinicius", "vinicius@teste.com", "vinicius");
 
-        Pessoa pessoa = context.getBean(Pessoa.class);
+        System.out.println("Deseja recuperar a senha? (S/N)");
+        String resposta = lerDados.nextLine();
+        if(resposta.equalsIgnoreCase("S")) {
+            ApplicationContext context = new AnnotationConfigApplicationContext(Programa.class);
+            UsuarioServico usuarioServico = context.getBean(UsuarioServico.class);
 
-        pessoa.setName("Vinicius");
-        pessoa.setIdade(29);
-        System.out.println(pessoa);
+            usuarioServico.setUsuario(usuario);
+            usuarioServico.lembrarSenhaPorEmail();
+
+        }
     }
 
 }
